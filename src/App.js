ec1ab74d1c0a678ea0b696d0c6ed038b8289152f
julweng <julweng@gmail.com>
2019-06-27 10:20:14 -0700

    remove unused code

--- src/App.js
+++ src/App.js
@@ -2,21 +2,10 @@ import React from "react"
 import { TodoContainer } from "components"
 import "./App.less"
 
-const todos = [
-  {
-    id: 1,
-    title: "have lunch with friends"
-  },
-  {
-    id: 2,
-    title: "practice react hooks"
-  }
-]
-
 const App = () => (
   <>
     <h1 className="App__Header">Hello World from React Redux boilerplate</h1>
-    <TodoContainer todos={todos} />
+    <TodoContainer />
   </>
 )
 
